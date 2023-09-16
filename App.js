47e200bd5c881aa7079fa4b1046fd25569165c3b
shubham8175 <shubham7868175@gmail.com>
2023-09-16 22:48:17 +0530

    The work of cloning NFT has started

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationContainer } from '@react-navigation/native';
+
+
+
+
+const Stack=createStackNavigator();
+const App = () => {
+  return (
+<View>
+  <Text>Ram</Text>
+</View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
