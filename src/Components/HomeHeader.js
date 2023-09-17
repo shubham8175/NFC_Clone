34fbf2b5c26801b353028900c526295f21031e1e
shubham8175 <shubham7868175@gmail.com>
2023-09-17 15:21:45 +0530

    HomeHeader is completed with profile and search button

--- src/Components/HomeHeader.js
+++ src/Components/HomeHeader.js
@@ -1,14 +1,84 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {Image, Text, View, TextInput} from 'react-native';
+import {COLORS, FONTS, SIZES, assets} from '../../constants';
 
-const HomeHeader = () => {
+const HomeHeader = ({onSearch}) => {
   return (
-    <View>
-      <Text>HomeHeader</Text>
+    <View style={{backgroundColor: COLORS.primary, padding: SIZES.font}}>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}>
+        <Image
+          source={assets.logo}
+          resizeMode="contain"
+          style={{width: 90, height: 25}}
+        />
+        <View style={{height: 45, width: 45}}>
+          <Image
+            source={assets.person01}
+            resizeMode="contain"
+            style={{height: '100%', width: '100%'}}
+          />
+          <Image
+            source={assets.badge}
+            resizeMode="contain"
+            style={{
+              position: 'absolute',
+              height: 15,
+              width: 15,
+              bottom: 0,
+              right: 0,
+            }}
+          />
+        </View>
+      </View>
+      <View style={{marginVertical: SIZES.font}}>
+        <Text
+          style={{
+            fontFamily: FONTS.regular,
+            fontSize: SIZES.small,
+            color: COLORS.white,
+          }}>
+          Hello Katrina 👋
+        </Text>
+        <Text
+          style={{
+            fontFamily: FONTS.bold,
+            fontSize: SIZES.large,
+            color: COLORS.white,
+            marginTop: SIZES.base / 2,
+          }}>
+          Lets find a Masterpiece
+        </Text>
+      </View>
+
+      <View style={{marginTop: SIZES.font}}>
+        <View
+          style={{
+            width: '100%',
+            borderRadius: SIZES.font,
+            backgroundColor: COLORS.gray,
+            flexDirection: 'row',
+            alignItems: 'center',
+            paddingHorizontal: SIZES.font,
+            paddingVertical: SIZES.small - 2,
+          }}>
+          <Image
+            source={assets.search}
+            resizeMode="contain"
+            style={{width: 20, height: 20, marginRight: SIZES.base}}
+          />
+          <TextInput
+            placeholder="Search"
+            style={{flex: 1}}
+            onChange={onSearch}
+          />
+        </View>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default HomeHeader
-
-const styles = StyleSheet.create({})
+export default HomeHeader;
